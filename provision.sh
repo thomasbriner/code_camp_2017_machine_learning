d0f224181596a7b505e43af5ae676d2dd9336f79
Andreas Schmidt <andreas.remigius@gmail.com>
2017-03-02 09:05:56 +0100

    Adding Vagrantfile and provision shell script

---
+++ provision.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+fix_lc_all_for_bash_rc() {
+  BASH_RC_PATH="$1"
+  if ! grep --quiet "LC_ALL" "$BASH_RC_PATH" ; then
+    logger --stderr "Setting LC_ALL=C in ${BASH_RC_PATH}"
+    echo "export LC_ALL=C" >> "${BASH_RC_PATH}"
+  fi
+}
+
+setup_pip() {
+  apt-get install -y python-pip
+  pip install --upgrade pip
+}
+
+install_pips() {
+  pip install NumPy
+  pip install SciPy
+  pip install scikit-learn
+}
+
+apt-get update
+
+fix_lc_all_for_bash_rc /root/.bashrc
+fix_lc_all_for_bash_rc /home/ubuntu/.bashrc
+
+setup_pip
+install_pips
+
