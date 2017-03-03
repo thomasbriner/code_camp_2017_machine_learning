dc597473c983174187d0f6e4509f6c841de657e8
Andreas Schmidt <andreas.remigius@gmail.com>
2017-03-03 11:56:26 +0100

    Reindented provision.sh to 4 spaces

--- provision.sh
+++ provision.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 
 fix_lc_all_for_bash_rc() {
-  BASH_RC_PATH="$1"
-  if ! grep --quiet "LC_ALL" "$BASH_RC_PATH" ; then
-    logger --stderr "Setting LC_ALL=C in ${BASH_RC_PATH}"
-    echo "export LC_ALL=C" >> "${BASH_RC_PATH}"
-  fi
+    BASH_RC_PATH="$1"
+    if ! grep --quiet "LC_ALL" "$BASH_RC_PATH" ; then
+	logger --stderr "Setting LC_ALL=C in ${BASH_RC_PATH}"
+	echo "export LC_ALL=C" >> "${BASH_RC_PATH}"
+    fi
 }
 
 setup_pip() {
-  apt-get install -y python-pip
-  pip install --upgrade pip
+    apt-get install -y python-pip
+    pip install --upgrade pip
 }
 
 install_pips() {
-  pip install NumPy
-  pip install SciPy
-  pip install scikit-learn
+    pip install NumPy
+    pip install SciPy
+    pip install scikit-learn
 }
 
 apt-get update
